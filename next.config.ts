f3946921f78884577ddf01c8717f7ef42a219864
csc <csc@cscdeMacBook-Pro.local>
2025-01-05 12:43:51 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import '@ant-design/v5-patch-for-react-19';
+const withTM = require('next-transpile-modules')(['antd']);
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withTM({
   /* config options here */
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+    turbo: {
+      loaders: {
+        '.less': ['style-loader', 'css-loader', 'less-loader'],
+      },
+    },
+  },
+
+});
 
 export default nextConfig;
