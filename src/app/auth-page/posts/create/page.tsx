8d96ed071ae88f55edf15dbc6310e781e4292da3
cscxxx <167551334+cscxxx@users.noreply.github.com>
2025-04-24 23:54:32 +0800

    优化创建界面，引入tinymic富文本编辑器

--- src/app/auth-page/posts/create/page.tsx
+++ src/app/auth-page/posts/create/page.tsx
@@ -19,11 +19,14 @@ import {
   Suspense,
   useActionState,
   useEffect,
+  useRef,
   useState,
 } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { createPost, getAllTags } from "../actions";
+import { Editor } from "@tinymce/tinymce-react";
+import clsx from "clsx";
 
 const FormSchema = z.object({
   title: z.string().min(2, {
@@ -37,6 +40,13 @@ const FormSchema = z.object({
 });
 
 export default function CreatePost() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current?.getContent());
+    }
+  };
+
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -61,6 +71,7 @@ export default function CreatePost() {
       <form
         onSubmit={form.handleSubmit((data) => {
           // formAction(data);
+          // console.log(data);
           createPost(data);
         })}
 
@@ -74,43 +85,61 @@ export default function CreatePost() {
         //   })();
         // }}
       >
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              {/* <FormLabel>Title</FormLabel> */}
-              <FormControl>
-                <Input placeholder="place input title ..." {...field} />
-              </FormControl>
-              {/* <FormDescription>
+        <div className="flex items-center py-2">
+          <div className="flex-1">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  {/* <FormLabel>Title</FormLabel> */}
+                  <FormControl>
+                    <Input placeholder="place input title ..." {...field} />
+                  </FormControl>
+                  {/* <FormDescription>
                 This is your public display name.
               </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="published"
-          render={({ field }) => (
-            <FormItem className="felx items-center justify-center ">
-              <FormLabel>发布</FormLabel>
-              <FormControl>
-                <Switch onCheckedChange={field.onChange} />
-              </FormControl>
-              {/* <FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="w-[100px] text-center">
+            <FormField
+              control={form.control}
+              name="published"
+              render={({ field }) => (
+                <FormItem className="felx items-center h-[36px]">
+                  <FormLabel className="leading-[36px]">发布</FormLabel>
+                  <FormControl>
+                    <Switch
+                      className="leading-[36px]"
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  {/* <FormDescription>
                 This is your public display name.
               </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="w-[100px] text-center ">
+            <Suspense fallback={<div>loading</div>}>
+              <Button type="submit" className="">
+                Submit
+              </Button>
+            </Suspense>
+          </div>
+        </div>
+
         <FormField
           control={form.control}
           name="tags"
           render={() => (
-            <FormItem className="flex items-center space-x-5 ">
+            <FormItem className="flex items-center space-x-5 rounded-md ">
+              <div></div>
               {tags.map((item) => {
                 return (
                   <FormField
@@ -120,11 +149,18 @@ export default function CreatePost() {
                     render={({ field }) => (
                       <FormItem
                         key={item.id}
-                        className="flex items-start space-x-1 "
+                        className={clsx(
+                          "flex items-start text-center !m-[0px] !mr-5 py-1 px-1 hover:bg-gray-300 rounded-md ",
+                          {
+                            "bg-blue-100":
+                              field.value?.includes(item.id.toString()) ??
+                              false,
+                          }
+                        )}
                       >
                         <FormControl>
                           <Checkbox
-                            className="my-1.5 "
+                            className="text-center h-[16px] space-y-0"
                             checked={
                               field.value?.includes(item.id.toString()) ?? false
                             }
@@ -143,7 +179,9 @@ export default function CreatePost() {
                             }}
                           />
                         </FormControl>
-                        <FormLabel>{item.desc}</FormLabel>
+                        <FormLabel className="cursor-pointer leading-[16px] h-[16px] !m-0 pl-1">
+                          {item.desc}
+                        </FormLabel>
                       </FormItem>
                     )}
                   ></FormField>
@@ -157,13 +195,54 @@ export default function CreatePost() {
           name="content"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Content</FormLabel>
+              {/* <FormLabel>Content</FormLabel> */}
               <FormControl>
-                <Textarea
+                <Editor
+                  apiKey="wjch2w8wj3lfna049cka51n2r8u2u4y9uanqwadks86758c2"
+                  onInit={(_evt, editor) => (editorRef.current = editor)}
+                  initialValue="<p>This is the initial content of the editor.</p>"
+                  value={field.value}
+                  onEditorChange={(content, editor) => {
+                    field.onChange(content);
+                  }}
+                  init={{
+                    height: 500,
+                    menubar: true,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "code",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                      "code",
+                      "help",
+                      "wordcount",
+                    ],
+                    toolbar:
+                      "undo redo | blocks | " +
+                      "bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist outdent indent | " +
+                      "removeformat | help",
+                    content_style:
+                      "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                  }}
+                />
+
+                {/* <Textarea
                   placeholder="place input content ..."
                   className="h-[400px] "
                   {...field}
-                />
+                /> */}
               </FormControl>
               {/* <FormDescription>
                 This is your public display name.
@@ -172,11 +251,6 @@ export default function CreatePost() {
             </FormItem>
           )}
         />
-        <Suspense fallback={<div>loading</div>}>
-          <Button type="submit" className="my-2">
-            Submit
-          </Button>
-        </Suspense>
       </form>
     </Form>
   );
